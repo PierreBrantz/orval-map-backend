18ce9532cc0fd8376f5bd8be0e05239a87a21f22
Pan <brantz.pierre@gmail.com>
2026-07-30 11:29:04 +0200

    fix: problem place type

---
+++ src/main/java/com/orvalmap/model/PlaceCreationDTO.java
@@ -0,0 +1,24 @@
+package com.orvalmap.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class PlaceCreationDTO {
+    @NotBlank(message = "Le nom est obligatoire")
+    private String name;
+
+    @NotBlank(message = "La ville est obligatoire")
+    private String city;
+
+    @NotNull(message = "Latitude obligatoire")
+    private Double lat;
+
+    @NotNull(message = "Longitude obligatoire")
+    private Double lng;
+
+    private Double price;
+
+    private PlaceType placeType; // Le type est optionnel, le service lui donnera une valeur par défaut si null
+}
