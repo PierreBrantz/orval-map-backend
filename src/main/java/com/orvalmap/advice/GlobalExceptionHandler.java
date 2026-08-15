25736e5a5963477bf7ffded8fcad0c86aabc2ca3
Pan <brantz.pierre@gmail.com>
2026-08-15 10:41:46 +0200

    compiling error

--- src/main/java/com/orvalmap/advice/GlobalExceptionHandler.java
+++ src/main/java/com/orvalmap/advice/GlobalExceptionHandler.java
@@ -2,25 +2,37 @@ package com.orvalmap.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Map<String, String>> handleIllegalStateException(IllegalStateException ex) {
-        // On vérifie si le message est celui de la distance pour être sûr
         if (ex.getMessage().contains("Vous n'êtes pas assez proche")) {
             Map<String, String> errorBody = Map.of("error", ex.getMessage());
             return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
         }
         
-        // Pour les autres IllegalStateException, on peut garder une erreur 500
         Map<String, String> errorBody = Map.of("error", "An unexpected error occurred");
         return new ResponseEntity<>(errorBody, HttpStatus.INTERNAL_SERVER_ERROR);
     }
